896404faa0e6a18dfa2fdc5690653fc8163afd8a
Kyle Filz <Kfilz@live.ca>
2021-03-17 21:55:59 -0500

    Fix migration.

--- src/migrations/weapons.ts
+++ src/migrations/weapons.ts
@@ -2,13 +2,12 @@ import { SWNRWeapon } from "../module/items/weapon";
 import { registerMigration } from "../migration";
 
 registerMigration(SWNRWeapon, "0.4.0", 0, (weapon, pastUpdates) => {
-  if (!weapon.data.data.secondStat)
-    pastUpdates["data.data.secondStat"] = "none";
+  if (!weapon.data.data.secondStat) pastUpdates["data.secondStat"] = "none";
   const stat = weapon.data.data.stat;
   if (stat.includes("/")) {
     const [first, second] = stat.split("/");
-    pastUpdates["data.data.stat"] = first;
-    pastUpdates["data.data.secondStat"] = second;
+    pastUpdates["data.stat"] = first;
+    pastUpdates["data.secondStat"] = second;
   }
   return pastUpdates;
 });
