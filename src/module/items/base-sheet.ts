2598d1987fd36a97c314da14a5db8878f41aef7b
Kyle Filz <Kfilz@live.ca>
2021-03-21 22:31:32 -0500

    Do base styling for item sheets.

--- src/module/items/base-sheet.ts
+++ src/module/items/base-sheet.ts
@@ -7,6 +7,14 @@ export class BaseSheet extends ItemSheet {
       tabs: [],
     });
   }
+
+  _injectHTML(html: JQuery<HTMLElement>, options: unknown): void {
+    html
+      .find(".window-content")
+      .addClass(["cq", "overflow-y-scroll", "relative"]);
+    super._injectHTML(html, options);
+  }
+
   _createEditor(target, editorOptions, initialContent): void {
     editorOptions.height = Math.max(editorOptions.height, 100);
     TextEditor.create(editorOptions, initialContent).then((mce) => {
