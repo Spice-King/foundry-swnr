89bdde5621e495dff24e0bd4d45a00a6da8f2424
Kyle Filz <Kfilz@live.ca>
2021-04-03 21:35:35 -0500

    Fix build tagging.

--- .github/workflows/set-version.js
+++ .github/workflows/set-version.js
@@ -8,8 +8,9 @@ if (!tagVersion || !(tagVersion.startsWith('v') || tagVersion.startsWith('b'))) 
   console.error(`Invalid version specified: ${tagVersion}`);
   process.exitCode = 1;
 } else {
-  manifest.version = tagVersion.substring(1); // strip the 'v'-prefix
-  manifest.download = `https://github.com/Spice-King/foundry-swnr/releases/download/${tagVersion}/swnr-${tagVersion}.zip`
+  version = tagVersion.substring(1); // strip the 'v'-prefix
+  manifest.version = version
+  manifest.download = `https://github.com/Spice-King/foundry-swnr/releases/download/${tagVersion}/swnr-v${version}.zip`
   fs.writeFileSync('src/system.yml', yaml.dump(manifest)); // pretty print JSON back to module.json
   console.log(tagVersion);
 }
