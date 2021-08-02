82cf05e4be44b9d1b857a35dde0af5a49e51fe7d
Kyle Filz <Kfilz@live.ca>
2021-08-02 17:27:49 -0500

    New base item/actor classes.

---
+++ src/module/base-actor.ts
@@ -0,0 +1,6 @@
+export class SWNRBaseActor<
+  Type extends Actor["type"] = Actor["type"]
+> extends Actor {
+  //@ts-expect-error Subtype override
+  data: Actor["data"] & { _source: { type: Type }; type: Type };
+}
--- src/module/base-item.ts
+++ src/module/base-item.ts
@@ -1,1 +1,6 @@
-export class SWNRBaseItem<D> extends Item<D> {}
+export class SWNRBaseItem<
+  Type extends Item["type"] = Item["type"]
+> extends Item {
+  //@ts-expect-error Subtype override
+  data: Item["data"] & { _source: { type: Type }; type: Type };
+}
