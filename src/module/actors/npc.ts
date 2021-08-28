2e6306ff288d9e8b84e0474082cf511804c58583
WintersleepAI <wintersleep@protonmail.com>
2021-08-28 10:07:20 -0500

    roll hit dice on create/drop token. do not roll if there is no HD

--- src/module/actors/npc.ts
+++ src/module/actors/npc.ts
@@ -6,6 +6,24 @@ export class SWNRNPCActor extends SWNRBaseActor<"npc"> {
     e.value = e.max - e.current - e.scene - e.day;
   }
 
+  // Set the max/value health based on D8 hit dice
+  rollHitDice(): void {
+    console.log("rolling NPC hit dice");
+    if (this.data.data.hitDice != null) {
+      console.log(`Updating health using ${this.data.data.hitDice} hit die `);
+      const roll = new Roll(`${this.data.data.hitDice}d8`).roll();
+      if (roll != undefined && roll.total != undefined){
+        const newHealth = roll.total;
+        console.log("Health now = ", roll.result);
+        this.update({"data.health.max": newHealth});
+        this.update({"data.health.value": newHealth});
+      }
+    }
+    else {
+      console.log("NPC has no hit dice, not rolling health");
+    }
+  }
+
   _onCreate(
     data: Parameters<SWNRBaseActor["_onCreate"]>[0],
     options: Parameters<SWNRBaseActor["_onCreate"]>[1],
@@ -13,8 +31,8 @@ export class SWNRNPCActor extends SWNRBaseActor<"npc"> {
   ): void {
     super._onCreate(data, options, userId);
     if (this.data["items"]["length"] || game.userId !== userId) return;
-    console.log("creating npc");
-    console.log(this.data);
+
+
     this.data.data["hitDice"]=1;
     // Can't figure out how to get this to work: this.actor.update({"data.hitDic": 1});
 
@@ -33,6 +51,13 @@ export class SWNRNPCActor extends SWNRBaseActor<"npc"> {
     ]);
   }
 }
+// Unsure of right types for TS here.
+Hooks.on("createToken", (document, options, userId) => {
+  if (document.actor?.type == "npc") {
+    document.actor.rollHitDice();
+  }
+});
+
 
 export const document = SWNRNPCActor;
 export const name = "npc";
