a3c2d0cb404e47b48fb749847cbaa0d076b00d47
WintersleepAI <wintersleep@protonmail.com>
2021-08-31 20:24:15 -0500

    working, but duplicate code

--- src/module/items/weapon.ts
+++ src/module/items/weapon.ts
@@ -16,6 +16,7 @@ export class SWNRWeapon extends SWNRBaseItem<"weapon"> {
   get hasAmmo(): boolean {
     return this.ammo.type === "none" || this.ammo.value > 0;
   }
+  
   async rollAttack(
     damageBonus: number,
     stat: number,
@@ -115,7 +116,7 @@ export class SWNRWeapon extends SWNRBaseItem<"weapon"> {
   }
 
   async rollForm(html: HTMLFormElement) {
-    console.log("Received ", html);
+    console.log("Received ", html , this);
     const form = <HTMLFormElement>html[0].querySelector("form");
     const modifier = parseInt(
       (<HTMLInputElement>form.querySelector('[name="modifier"]'))?.value
@@ -128,15 +129,25 @@ export class SWNRWeapon extends SWNRBaseItem<"weapon"> {
     const skillId =
       (<HTMLSelectElement>form.querySelector('[name="skill"]'))?.value ||
       this.data.data.skill;
-    const skill = this.actor?.getEmbeddedDocument(
+
+    const actorId = (<HTMLSelectElement>form.querySelector('[name="actorId"]'))?.value;
+    const statName = (<HTMLSelectElement>form.querySelector('[name="statName"]'))?.value;
+    
+    const actor = game.actors?.get(actorId);
+    if (!actor) {
+      console.log("Error actor no longer exists ", actorId);
+      return;
+    }
+
+    const skill = actor.getEmbeddedDocument(
       "Item",
       skillId
     ) as SWNRBaseItem<"skill">;
 
-    const stat = this.actor?.data.data["stats"][this.data.data.stat] || {
+    const stat = actor.data.data["stats"][statName] || {
       mod: 0,
     };
-    console.log("look up", this.actor, skillId, skill,stat);
+    console.log("look up", actor, skillId, skill,stat);
     // 1d20 + attack bonus (PC plus weapon) + skill mod (-2 if untrained)
     // weapon dice + stat mod + skill if enabled or punch.
     // shock: damage + stat
@@ -154,7 +165,75 @@ export class SWNRWeapon extends SWNRBaseItem<"weapon"> {
       effectiveSkillRank:
         skill.data.data.rank < 0 ? -2 : skill.data.data.rank,
     };
-    console.log(rollData);
+    console.log(rollData, this);
+    return this.rollWeaponHit(rollData);
+  }
+
+  async rollWeaponHit(rollData) {
+    const template = "systems/swnr/templates/chat/attack-roll.html";
+    if (!this.actor) {
+      console.log("missing actor");
+      return;
+    }
+    const hitRoll = new Roll(
+      "1d20 + @burstFire + @modifier + @actor.ab + @weapon.ab + @stat.mod + @effectiveSkillRank",
+      rollData
+    ).roll();
+    rollData.hitRoll = +(hitRoll.dice[0].total?.toString() ?? 0);
+    const damageRoll = new Roll(
+      this.data.data.damage +
+        " + @burstFire + @stat.mod" +
+        (this.data.data.skillBoostsDamage
+          ? ` + ${rollData.skill.data.data.rank}`
+          : ""),
+      rollData
+    ).roll();
+    const diceTooltip = {
+      hit: await hitRoll.render(),
+      damage: await damageRoll.render(),
+    };
+    const dialogData = {
+      actor: this.actor,
+      weapon: this,
+      skill: rollData.skill,
+      hitRoll,
+      stat: rollData.stat,
+      damageRoll,
+      burstFire: rollData.burstFire,
+      modifier: rollData.modifier,
+      effectiveSkillRank: rollData.effectiveSkillRank,
+      diceTooltip,
+      ammoRatio: Math.clamped(
+        Math.floor(
+          (this.data.data.ammo.value * 20) / this.data.data.ammo.max
+        ),
+        0,
+        20
+      ),
+    };
+    const rollMode = game.settings.get("core", "rollMode");
+    const diceData = Roll.fromTerms([
+      PoolTerm.fromRolls([hitRoll, damageRoll]),
+    ]);
+    if (this.data.data.ammo.type !== "none") {
+      const newAmmoTotal = this.data.data.ammo.value - 1 - rollData.burstFire;
+      await this.update({ "data.ammo.value": newAmmoTotal }, {});
+      if (newAmmoTotal === 0)
+        ui.notifications?.warn(`Your ${this.name} is now out of ammo!`);
+    }
+    const chatContent = await renderTemplate(template, dialogData);
+    const chatMessage = getDocumentClass("ChatMessage");
+    chatMessage.create(
+      chatMessage.applyRollMode(
+        {
+          speaker: ChatMessage.getSpeaker({ actor: this.actor }),
+          roll: JSON.stringify(diceData),
+          content: chatContent,
+          type: CONST.CHAT_MESSAGE_TYPES.ROLL,
+        },
+        rollMode
+      )
+    );
   }
 
   async roll(): Promise<void> {
@@ -183,12 +262,71 @@ export class SWNRWeapon extends SWNRBaseItem<"weapon"> {
       weapon: this.data.data,
       skills: this.actor.itemTypes.skill,
       stat: this.actor.data.data["stats"][this.data.data.stat],
+      statName: this.data.data.stat,
       skill: this.data.data.skill,
       burstFireHasAmmo,
     };
+    console.log(dialogData);
     const template = "systems/swnr/templates/dialogs/roll-attack.html";
     const html = await renderTemplate(template, dialogData);
-    //this.popUpDialog?.close();
+    
+    
+    const _rollForm  = async (html: HTMLFormElement) => {
+      console.log("Received ", html , this);
+      const form = <HTMLFormElement>html[0].querySelector("form");
+      const modifier = parseInt(
+        (<HTMLInputElement>form.querySelector('[name="modifier"]'))?.value
+      );
+      const burstFire = (<HTMLInputElement>(
+        form.querySelector('[name="burstFire"]')
+      ))?.checked
+        ? 2
+        : 0;
+      const skillId =
+        (<HTMLSelectElement>form.querySelector('[name="skill"]'))?.value ||
+        this.data.data.skill;
+  
+      const actorId = (<HTMLSelectElement>form.querySelector('[name="actorId"]'))?.value;
+      const statName = (<HTMLSelectElement>form.querySelector('[name="statName"]'))?.value;
+      
+      const actor = game.actors?.get(actorId);
+      if (!actor) {
+        console.log("Error actor no longer exists ", actorId);
+        return;
+      }
+  
+      const skill = actor.getEmbeddedDocument(
+        "Item",
+        skillId
+      ) as SWNRBaseItem<"skill">;
+  
+      const stat = actor.data.data["stats"][statName] || {
+        mod: 0,
+      };
+      console.log("look up", actor, skillId, skill,stat);
+      // 1d20 + attack bonus (PC plus weapon) + skill mod (-2 if untrained)
+      // weapon dice + stat mod + skill if enabled or punch.
+      // shock: damage + stat
+      // const skill = this.actor.items.filter(w => w.)
+      // Burst is +2 To hit and to damage
+  
+      const rollData = {
+        actor: this.actor?.getRollData(),
+        weapon: this.data,
+        stat,
+        skill: skill,
+        hitRoll: <number | undefined>undefined,
+        burstFire,
+        modifier,
+        effectiveSkillRank:
+          skill.data.data.rank < 0 ? -2 : skill.data.data.rank,
+      };
+      console.log(rollData, this);
+      return this.rollWeaponHit(rollData);
+    }
+  
+    
+    this.popUpDialog?.close();
     this.popUpDialog = new ValidatedDialog(
       {
         title: title,
@@ -197,7 +335,7 @@ export class SWNRWeapon extends SWNRBaseItem<"weapon"> {
         buttons: {
           roll: {
             label: game.i18n.localize("swnr.chat.roll"),
-            callback: this.rollForm,
+            callback: _rollForm,
           },
         },
       },
@@ -208,6 +346,7 @@ export class SWNRWeapon extends SWNRBaseItem<"weapon"> {
         classes: ["swnr"],
       }
     );
+    console.log("t1", this);
     const s = this.popUpDialog.render(true);
 
   }
